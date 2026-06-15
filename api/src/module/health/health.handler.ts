a4b739ae020ffde9188eb17e00d487f134da43d3
pranandayoga <pranandayoga21@gmail.com>
2026-06-15 15:42:32 +0800

    feat(api): Implement health check service and response handler

--- api/src/module/health/health.handler.ts
+++ api/src/module/health/health.handler.ts
@@ -1,7 +1,12 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { HEALTH_ROUTES } from "./health.routes.js";
+import { HEALTH } from "./health.service.js";
 
 export const HEALTH_HANDLER = new OpenAPIHono().openapi(
   HEALTH_ROUTES.DETECT,
-  async (c) => {},
+  async (c) => {
+    const result = await HEALTH();
+
+    return c.json(result, 200);
+  },
 );
